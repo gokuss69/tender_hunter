4c889cb9ce1daee5e117072b3f181e63c156bd77
gokuss69 <gokunitro@protonmail.com>
2026-02-28 19:05:00 +0530

    Update sites.js

--- sites.js
+++ sites.js
@@ -1,6 +1,162 @@
 module.exports = [
-  {
-    name: "Example Site",
-    url: "https://example.com"
-  }
+
+/* ==========================================
+   REGIONAL E-PROCUREMENT (MOST IMPORTANT)
+========================================== */
+
+{
+  name: "Punjab eProcurement",
+  url: "https://eproc.punjab.gov.in/nicgep/app"
+},
+
+{
+  name: "J&K eProcurement",
+  url: "https://jktenders.gov.in/nicgep/app"
+},
+
+{
+  name: "Himachal eProcurement",
+  url: "https://hptenders.gov.in/nicgep/app"
+},
+
+{
+  name: "Chandigarh eTender",
+  url: "https://etenders.chd.nic.in"
+},
+
+
+/* ==========================================
+   CENTRAL PROCUREMENT (REGIONALLY RELEVANT)
+========================================== */
+
+{
+  name: "GeM Bid Portal",
+  url: "https://bidplus.gem.gov.in/all-bids"
+},
+
+{
+  name: "Central Public Procurement Portal",
+  url: "https://eprocure.gov.in/eprocure/app"
+},
+
+
+/* ==========================================
+   CSIR LABS (VERY HIGH VALUE CLIENTS)
+========================================== */
+
+{
+  name: "CSIR-IIIM Jammu",
+  url: "https://iiim.res.in/tenders"
+},
+
+{
+  name: "CSIR-CSIO Chandigarh",
+  url: "https://www.csio.res.in/Common.aspx?DS=Tenders"
+},
+
+{
+  name: "CSIR-IMTECH Chandigarh",
+  url: "https://www.imtech.res.in/tenders"
+},
+
+{
+  name: "CSIR-IHBT Palampur",
+  url: "https://www.ihbt.res.in/en/tenders"
+},
+
+
+/* ==========================================
+   IIT / PREMIER INSTITUTES
+========================================== */
+
+{
+  name: "IIT Ropar",
+  url: "https://www.iitrpr.ac.in/tenders"
+},
+
+{
+  name: "IISER Mohali",
+  url: "https://www.iisermohali.ac.in/tenders"
+},
+
+{
+  name: "NIPER Mohali",
+  url: "https://www.niper.gov.in/tenders"
+},
+
+
+/* ==========================================
+   MEDICAL & HEALTH RESEARCH
+========================================== */
+
+{
+  name: "PGIMER Chandigarh",
+  url: "https://pgimer.edu.in"
+},
+
+{
+  name: "AIIMS Jammu",
+  url: "https://aiimsjammu.edu.in/tenders"
+},
+
+{
+  name: "AIIMS Bathinda",
+  url: "https://aiimsbathinda.edu.in/tenders"
+},
+
+
+/* ==========================================
+   AGRICULTURE & FOOD SCIENCE (BIG BUYERS)
+========================================== */
+
+{
+  name: "PAU Ludhiana",
+  url: "https://www.pau.edu/tenders.php"
+},
+
+{
+  name: "SKUAST Jammu",
+  url: "https://skuast.org/site/Tenders.aspx"
+},
+
+{
+  name: "CSKHPKV Palampur",
+  url: "https://hillagric.ac.in/info/tenders.php"
+},
+
+
+/* ==========================================
+   UNIVERSITIES (ACTIVE RESEARCH BUYERS)
+========================================== */
+
+{
+  name: "Punjab University Chandigarh",
+  url: "https://puchd.ac.in/tenders.php"
+},
+
+{
+  name: "University of Jammu",
+  url: "https://www.jammuuniversity.ac.in/tenders"
+},
+
+{
+  name: "Himachal Pradesh University",
+  url: "https://www.hpuniv.ac.in/university-detail/tender"
+},
+
+{
+  name: "GNDU Amritsar",
+  url: "https://online.gndu.ac.in/tender-notice.aspx"
+},
+
+
+/* ==========================================
+   DEFENCE / STRATEGIC (REGIONALLY ACTIVE)
+========================================== */
+
+{
+  name: "DRDO Tenders",
+  url: "https://www.drdo.gov.in/tenders"
+}
+
 ];
